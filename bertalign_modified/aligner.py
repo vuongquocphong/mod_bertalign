59a43afe074b43dba578ed71d0678193056f3932
vuongquocphong <vuongquocphongv@gmail.com>
2025-05-15 16:05:50 +0000

    add modified bertalign

---
+++ bertalign_modified/aligner.py
@@ -0,0 +1,142 @@
+from concurrent.futures import ThreadPoolExecutor
+import time
+import numpy as np
+
+from bertalign import model
+from bertalign.corelib import *
+from bertalign.utils import *
+
+class Bertalign:
+    def __init__(self,
+                 src,
+                 tgt,
+                 model = model,
+                 max_align=5,
+                 top_k=3,
+                 win=5,
+                 skip=-0.1,
+                 margin=True,
+                 len_penalty=True,
+                 sentence_num_penalty=True,
+                 union_score=True,
+                 is_split=False,
+                 ner_dict={}
+               ):
+        self.src = src
+        self.model = model
+        self.max_align = max_align
+        self.top_k = top_k
+        self.win = win
+        self.skip = skip
+        self.margin = margin
+        self.len_penalty = len_penalty
+        self.sentence_num_penalty = sentence_num_penalty
+        self.union_score = union_score
+        self.ner_dict = ner_dict
+        
+        src = clean_text(src)
+        tgt = clean_text(tgt)
+        src_lang = 'zh'
+        tgt_lang = 'vi'
+        
+        # Split into sentences
+        if is_split:
+            src_sents = src.splitlines()
+            tgt_sents = tgt.splitlines()
+        else:
+            src_sents = split_sents(src, src_lang)
+            tgt_sents = split_sents(tgt, tgt_lang)
+
+        # Get the number of sentences
+        src_num = len(src_sents)
+        tgt_num = len(tgt_sents)
+        
+        print("Source language: {}, Number of sentences: {}".format(src_lang, src_num))
+        print("Target language: {}, Number of sentences: {}".format(tgt_lang, tgt_num))
+
+        # Convert sentences into embeddings
+        print("Embedding source and target text using {} ...".format(self.model.model_name))
+        src_vecs, src_lens = self.model.transform(src_sents, max_align - 1)
+        tgt_vecs, tgt_lens = self.model.transform(tgt_sents, max_align - 1)
+
+        char_ratio = np.sum(src_lens[0,]) / np.sum(tgt_lens[0,])
+
+        self.src_lang = src_lang
+        self.tgt_lang = tgt_lang
+        self.src_sents = src_sents
+        self.tgt_sents = tgt_sents
+        self.src_num = src_num
+        self.tgt_num = tgt_num
+        self.src_lens = src_lens
+        self.tgt_lens = tgt_lens
+        self.char_ratio = char_ratio
+        self.src_vecs = src_vecs
+        self.tgt_vecs = tgt_vecs
+
+    def align_sents(self):
+
+        print("Preparing words list ...")
+        converted_src, converted_tgt, src_word_len, tgt_word_len = self._prepare_words_list()
+
+        print("Performing first-step alignment ...")
+        D, I = find_top_k_sents(self.src_vecs[0,:], self.tgt_vecs[0,:], k=self.top_k)
+        first_alignment_types = get_alignment_types(2) # 0-1, 1-0, 1-1
+        first_w, first_path = find_first_search_path(self.src_num, self.tgt_num)
+        src_keys = list(self.ner_dict.keys())
+        tgt_keys = list(self.ner_dict.values())
+        first_pointers = first_pass_align(self.src_num, self.tgt_num, first_w, first_path, first_alignment_types, D, I, src_sents=self.src_sents, tgt_sents=self.tgt_sents, src_keys=src_keys, tgt_keys=tgt_keys)
+        first_alignment = first_back_track(self.src_num, self.tgt_num, first_pointers, first_path, first_alignment_types)
+
+        print("Performing second-step alignment ...")
+        second_alignment_types = get_alignment_types(self.max_align)
+        second_w, second_path = find_second_search_path(first_alignment, self.win, self.src_num, self.tgt_num)
+        # second_pointers = second_pass_align(self.src_vecs, self.tgt_vecs, self.src_lens, self.tgt_lens,
+        #                                     second_w, second_path, second_alignment_types,
+        #                                     self.char_ratio, self.skip, margin=self.margin, len_penalty=self.len_penalty)
+        second_pointers = second_pass_align(self.src_vecs, self.tgt_vecs, self.src_lens, self.tgt_lens,
+                                            converted_src, converted_tgt, src_word_len, tgt_word_len,
+                                            second_w, second_path, second_alignment_types,
+                                            self.char_ratio, self.skip, margin=self.margin, len_penalty=self.len_penalty, sentence_num_penalty=self.sentence_num_penalty, union_score=self.union_score)
+        second_alignment = second_back_track(self.src_num, self.tgt_num, second_pointers, second_path, second_alignment_types)
+        
+        print("Finished! Successfully aligning {} {} sentences to {} {} sentences\n".format(self.src_num, self.src_lang, self.tgt_num, self.tgt_lang))
+        self.result = second_alignment
+    
+    def print_sents(self):
+        for bead in (self.result):
+            src_line = self._get_line(bead[0], self.src_sents)
+            tgt_line = self._get_line(bead[1], self.tgt_sents)
+            print(src_line + "\n" + tgt_line + "\n")
+
+    def _prepare_words_list(self):
+
+        start_time = time.time()
+
+        # Convert zh text to words list
+        converted_src, src_word_len = convert_zh(self.src, self.max_align - 1)
+        converted_zh_len = len(converted_src[0])
+
+        # Prepare index dictionary of each words
+        words_index = convert_words_to_indexList(converted_src, self.max_align - 1)
+
+        # # Check whether the number of converted_src and src_num are equal
+        if converted_zh_len != self.src_num:
+            print("Error: The number of converted source sentences does not match the number of source sentences.")
+            print("Converted source sentences: {}".format(converted_zh_len))
+            print("Source sentences: {}".format(self.src_num))
+            raise ValueError("The number of converted source sentences does not match the number of source sentences.")
+
+        # Convert vn text to words list
+        converted_tgt, tgt_word_len = convert_vn(self.tgt_sents, self.max_align - 1)
+
+        end_time = time.time()
+        print("Time taken to convert sentences: {:.2f} seconds".format(end_time - start_time))
+
+        return words_index, converted_tgt, src_word_len, tgt_word_len
+
+    @staticmethod
+    def _get_line(bead, lines):
+        line = ''
+        if len(bead) > 0:
+            line = ' '.join(lines[bead[0]:bead[-1]+1])
+        return line
---
+++ bertalign_modified/utils.py
@@ -0,0 +1,251 @@
+from collections import defaultdict
+from itertools import chain
+import re
+import xmlrpc.client
+from sentence_splitter import SentenceSplitter
+from underthesea import sent_tokenize
+
+def clean_text(text):
+	clean_text = []
+	text = text.strip()
+	lines = text.splitlines()
+	for line in lines:
+		line = line.strip()
+		if line:
+			line = re.sub('\s+', ' ', line)
+			clean_text.append(line)
+	return "\n".join(clean_text)
+	
+def split_sents(text, lang):
+	if lang == 'zh':
+		sents = _split_zh(text)
+	else:
+		sents = sent_tokenize(text)
+		sents = [sent.strip() for sent in sents]
+	return sents
+	
+def _split_zh(text, limit=1000):
+	sent_list = []
+	text = re.sub('(?P<quotation_mark>([。.？！](?![”’"」\'）])))', r'\g<quotation_mark>\n', text)
+	text = re.sub('(?P<quotation_mark>([。.？！]|…{1,2})[”’"」\'）])', r'\g<quotation_mark>\n', text)
+	sent_list_ori = text.splitlines()
+	for sent in sent_list_ori:
+		sent = sent.strip()
+		if not sent:
+			continue
+		else:
+			while len(sent) > limit:
+				temp = sent[0:limit]
+				sent_list.append(temp)
+				sent = sent[limit:]
+			sent_list.append(sent)
+	return sent_list
+		
+def yield_overlaps(lines, num_overlaps):
+	lines = [_preprocess_line(line) for line in lines]
+	for overlap in range(1, num_overlaps + 1):
+		for out_line in _layer(lines, overlap):
+			# check must be here so all outputs are unique
+			out_line2 = out_line[:10000]  # limit line so dont encode arbitrarily long sentences
+			yield out_line2
+
+def _layer(lines, num_overlaps, comb=' '):
+	if num_overlaps < 1:
+		raise Exception('num_overlaps must be >= 1')
+	out = ['PAD', ] * min(num_overlaps - 1, len(lines))
+	for ii in range(len(lines) - num_overlaps + 1):
+		out.append(comb.join(lines[ii:ii + num_overlaps]))
+	return out
+	
+def _preprocess_line(line):
+	line = line.strip()
+	if len(line) == 0:
+		line = 'BLANK_LINE'
+	return line
+
+###########################################################################
+# UNION PREPARATION
+###########################################################################
+
+def _post_request_to_api( data: str ) -> list[str]:
+	"""
+	Sends a POST request to the specified API.
+
+	:param data: The text need to convert to sino-vietnamese.
+	:return: The list of sino-converted of each sentence.
+	"""
+	def batch_transliterate(sentences, server_url="http://localhost:8080/RPC2"):
+		server = xmlrpc.client.ServerProxy(server_url)
+		results = []
+
+		for sentence in sentences:
+			params = {'text': sentence}
+			try:
+				response = server.translate(params)
+				results.append(response.get('text', ''))  # fallback to empty string if 'text' missing
+			except Exception as e:
+				results.append(f"[Error: {e}]")  # include error for debugging
+		return results
+	
+	def preprocess_snt_for_transliteration(replace_dict, text: str):
+		# Preprocess the text if necessary (e.g., remove unwanted characters)
+		for char in text:
+			# Check if the character is in Basic Multilingual Plane (BMP)
+			if ord(char) <= 65535:
+				continue
+			# If not, check if it is in the replace dictionary
+			if char in replace_dict.keys():
+				text = text.replace(char, replace_dict[char])
+				print(replace_dict[char])
+			else:
+				# If the character is not in the replace dictionary, replace it with a space
+				text = text.replace(char, ' ')
+		return text
+	
+	lines = _split_zh(data, limit=1000)
+
+	for line in lines:
+		line = ' '.join(line)
+	
+	nom_dict = {
+	    '𦏁': 'hi',
+	}
+	
+	preprocessed_lines = [preprocess_snt_for_transliteration(nom_dict, line) for line in lines]
+
+	transliterated_lines = batch_transliterate(preprocessed_lines)
+
+	return transliterated_lines
+	
+def _clean_zh_text(text: str) -> str:
+	"""
+	Cleans the input text by removing unwanted characters.
+
+	:param text: The input text to clean.
+	:return: The cleaned text.
+	"""
+	# Define a regex pattern to remove unwanted characters
+	pattern = r"[。！？；：，—“”‘’《》【】（）；,;:.!?]"
+	return re.sub(pattern, '', text)
+
+def _clean_vietnamese_text(text: str) -> str:
+	"""
+	Cleans the Vietnamese text by removing unwanted characters.
+
+	:param text: The input text to clean.
+	:return: The cleaned text.
+	"""
+	# Define a regex pattern to match Vietnamese sentence marks
+	pattern = r"[.!?；：，—“”‘’\[\]\(\),:;\"]"
+	return re.sub(pattern, ' ', text)
+
+def convert_zh(text: str, overlaps: int):
+	"""
+	Convert the input text to sino-vietnamese using the API.
+
+	:param text: The input text to convert.
+	:return: A list of sino-converted sentences.
+	"""
+
+	converted_sentences = _post_request_to_api(text)
+	if converted_sentences is None:
+		raise Exception("Error in API response.")
+	
+	# Initiate the words list
+	words = [[] for _ in range(overlaps)]
+
+	# First layer
+	words[0].extend(
+		[
+			[word.strip() for word in _clean_zh_text(sentence).split() if word.strip()]
+			for sentence in converted_sentences
+		]
+	)
+
+	num_sent = len(words[0])
+
+	# Remaining layers	  
+	for layer in range(2, overlaps + 1):
+		index = layer - 1
+		for sent in words[layer - 2]:
+			if index >= num_sent: break
+			words[layer - 1].append(sent + words[0][index])
+			index += 1
+	
+	# Add PAD for all layers
+	for layer in range(2, overlaps + 1):
+		words[layer - 1] = [['PAD']] * min( layer - 1, num_sent) + words[layer - 1]
+
+	# Create words length list
+	src_words_len = [[] for _ in range(overlaps)]
+	for layer in range(overlaps):
+		src_words_len[layer] = [len(sent) for sent in words[layer]]
+	
+	return words, src_words_len
+
+def convert_vn(tgt: list[str], overlaps: int) -> list[list[str]]:
+	"""
+	Convert the input text to sino-vietnamese using the API.
+
+	:param text: The input text to convert.
+	:return: A list of sino-converted sentences.
+	"""
+	# Initiate the words list
+	result = [[] for _ in range(overlaps)]
+
+	# First layer
+	result[0].extend(
+		[
+			[word.strip() for word in _clean_vietnamese_text(sentence).split() if word.strip()]
+			for sentence in tgt
+		]
+	)
+
+	num_sent = len(result[0])
+
+	# Remaining layers	  
+	for layer in range(2, overlaps + 1):
+		index = layer - 1
+		for sent in result[layer - 2]:
+			if index >= num_sent: break
+			result[layer - 1].append(sent + result[0][index])
+			index += 1
+	
+	# Add PAD for all layers
+	for layer in range(2, overlaps + 1):
+		result[layer - 1] = [['PAD']] * min( layer - 1, num_sent) + result[layer - 1]
+
+	# Create words length list
+	tgt_words_len = [[] for _ in range(overlaps)]
+	for layer in range(overlaps):
+		tgt_words_len[layer] = [len(sent) for sent in result[layer]]
+	
+	return result, tgt_words_len
+
+def _create_dict_from_list(lst: list[str]) -> dict[str, list[int]]:
+	"""
+	Creates a dictionary from a list of words.
+
+	:param lst: The list of words.
+	:return: A dictionary with words as keys and their indices as values.
+	"""
+	reserve = defaultdict(list[int])
+	for index, word in enumerate(lst):
+		reserve[word].append(index)
+
+	for word in reserve:
+		reserve[word].reverse()
+
+	return reserve
+
+def convert_words_to_indexList(words: list[list[str]], overlaps: int) -> list[dict[str, list[int]]]:
+	"""
+	Convert the words to index list.
+
+	:param words: The list of words.
+	:return: A list of dictionaries with words as keys and their indices as values.
+	"""
+	result = [[] for _ in range(overlaps)]
+	for layer in range(overlaps):
+		result[layer] = [_create_dict_from_list(sent) for sent in words[layer]]
+	return result
