0d8c30933b234d0bccff782ae873875ad9c17b28
Van Phuc Luu <94552405+hoktro18@users.noreply.github.com>
2025-08-01 17:43:40 +0700

    Fix: merging RegEx

--- bertalign/utils.py
+++ bertalign/utils.py
@@ -47,18 +47,13 @@ def split_sents(text, lang):
 	refine_sents = [sents[-1]] 
 	index = len(sents) - 2
 	while index >= 0:
-		if not re.match(r'^.*?:?\s*\d+\s*\.$', sents[index]):
-			refine_sents.append(sents[index])
+		if re.match(r'^.*?:\s*\d+\s*\.$', sents[index]) or re.fullmatch(r'\d+\s*\.', sents[index]):
+			refine_sents[-1] = sents[index] + ' ' + refine_sents[-1]
+			print(refine_sents[-1])
 			index -= 1
 			continue
-
-		if not re.match(r'^\d+\s*\.$', sents[index]):
-			refine_sents.append(sents[index])
-			index -= 1
-			continue
-
-		refine_sents[-1] = sents[index] + ' ' + refine_sents[-1]
-		index -= 1
+		refine_sents.append(sents[index])
+		index -= 1		
 	
 	refine_sents.reverse()
 	return refine_sents
